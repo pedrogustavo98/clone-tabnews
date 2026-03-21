5cdab53692c286510eb840f4afccef6bdc81113d
PEDRO SOUZA <77795299+pedrogustavo98@users.noreply.github.com>
2026-03-21 21:49:16 +0000

    simples alteracao

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mãe, eu amo você! Se você me ama, então da uma risadinha!</h1>
+    return <h1>Mãe, eu amo você! Se você me ama, então da uma risadinha! 😍​</h1>
 }
 
 export default Home;
