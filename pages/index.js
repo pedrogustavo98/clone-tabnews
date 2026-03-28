e09af356b445d208d14dd5a75438a0bb4de3b805
PEDRO SOUZA <77795299+pedrogustavo98@users.noreply.github.com>
2026-03-28 21:52:38 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Mãe, eu amo você! Se você me ama, então da uma risadinha! 😍​</h1>
+  return <h1>Mãe, eu amo você! Se você me ama, então da uma risadinha! 😍​</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
