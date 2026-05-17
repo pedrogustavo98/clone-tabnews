b16a71b38f816463dd7659d3ff6c2e79e6299ea9
PEDRO SOUZA <77795299+pedrogustavo98@users.noreply.github.com>
2026-05-17 22:56:12 +0000

    alteracao da view

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 function Home() {
 
-  let teste = "<h1>Olá, essa aqui é apenas uma página de teste! HEHEHE</h1";
+  teste = "<h1>";
+  teste += "Olá, essa aqui é apenas uma página de teste! HEHEHE";
   teste += '<br><br>Simples teste simples';
+  teste += "</h1>";
 
   return teste;
 }
