62e6d53210962f4d584fe3cd1f5740e833b32a32
PEDRO SOUZA <77795299+pedrogustavo98@users.noreply.github.com>
2026-05-17 22:46:23 +0000

    teste de alteracao no html

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Olá, essa aqui é apenas uma página de teste! <br>HEHEHE</br>​</h1>;
+  return <h1>Olá, essa aqui é apenas uma página de teste! HEHEHE</h1>;
 }
 
 export default Home;
