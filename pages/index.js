9919edfa456f4df184e638a92adaaf5990a00446
PEDRO SOUZA <77795299+pedrogustavo98@users.noreply.github.com>
2026-05-17 22:53:28 +0000

    teste do html

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Olá, essa aqui é apenas uma página de teste! HEHEHE</h1>;
+
+  let teste = "<h1>Olá, essa aqui é apenas uma página de teste! HEHEHE</h1";
+  teste += '<br><br>Simples teste simples';
+
+  return teste;
 }
 
 export default Home;
