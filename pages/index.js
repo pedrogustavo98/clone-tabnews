86d10279c70dc79a8e1674bd04677c85cb66149b
PEDRO SOUZA <77795299+pedrogustavo98@users.noreply.github.com>
2026-05-17 23:04:24 +0000

    teste de alteracao

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ function Home() {
   let teste = "<h1>";
   teste += "Olá, essa aqui é apenas uma página de teste! HEHEHE";
   teste += "</h1>";
-  teste += '<br><br>Simples teste simples';
+  teste += "<br><br>Simples teste simples";
 
   return teste;
 }
