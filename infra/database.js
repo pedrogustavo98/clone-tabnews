450e3df6881264df75134bb3374b7137ac01175c
PEDRO SOUZA <77795299+pedrogustavo98@users.noreply.github.com>
2026-05-26 04:10:42 +0000

    configurando comunicação com o banco de dados

---
+++ infra/database.js
@@ -0,0 +1,20 @@
+import {Client} from "pg";
+
+async function query (queryObject) {
+    const client = new Client({
+        host: "localhost",
+        port: 5432,
+        user: "myuser",
+        database: "mydb",
+        password: "local_password",
+    });
+    await client.connect();
+    const result = await client.query(queryObject);
+    await client.end();
+    return result;
+}
+
+
+export default {
+    query: query,
+};
