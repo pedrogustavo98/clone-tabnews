c0748f3a25850d433e8c65a1ad0e20530e1e6aa5
PEDRO SOUZA <77795299+pedrogustavo98@users.noreply.github.com>
2026-05-28 19:36:06 +0000

    ajuste credencias banco de dados

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,7 +3,8 @@ import database from "../../../../infra/database";
 async function handler(request, response) {
     try {
         const result = await database.query("SELECT 1 + 2 AS sum ");
-        response.status(200).json({chave: "são acima da média", resultado: result.rows[0].sum});
+        let variavel = process.env;
+        response.status(200).json({chave: "são acima da média", resultado: result.rows[0].sum, 'variavel_ambiente': variavel});
     } catch (error) {
         console.error("Erro na API:", error);
         response.status(500).json({erro: error.message});
